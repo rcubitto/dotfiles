1c27a4ddaebbfd1cbfa0b1206de04422b48add77
Raul Cubitto <rcubitto@gmail.com>
2021-02-01 11:22:37 -0300

    wip

--- aliases.zsh
+++ aliases.zsh
@@ -27,3 +27,14 @@ alias gpod="git push origin dev"
 alias gpos="git push origin staging"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias wip="git add --all; git commit -m 'wip'"
+
+slugify_dir () {
+	cd $1
+    for f in *;
+    do
+    	slugFilename=$(slugify ${f%.*})
+        slugExtension=$(slugify ${f##*.})
+        mv $f "${slugFilename}.${slugExtension}"
+	done
+    cd -
+}
